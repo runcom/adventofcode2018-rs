135928c5fd9dee838cb03c19ebe6c4b6c37ac2a1
Antonio Murdaca <runcom@linux.com>
2018-12-16 22:25:07 +0100

    don't be silly
    Signed-off-by: Antonio Murdaca <runcom@linux.com>

--- 1/main2.rs
+++ 1/main2.rs
@@ -7,11 +7,14 @@ fn main() {
     let mut solution: i32 = 0;
     let mut dup = HashMap::new();
     let mut dupfound = false;
+    let f = File::open("input.txt").unwrap();
+    let file = BufReader::new(&f);
+    let mut lines: Vec<String> = Vec::new();
+    for line in file.lines() {
+        lines.push(line.unwrap());
+    }
     while !dupfound {
-        let f = File::open("input.txt").unwrap();
-        let file = BufReader::new(&f);
-        for line in file.lines() {
-            let l = line.unwrap();
+        for l in &lines{
             let op = l.chars().next().unwrap();
             let num = &l[1..l.len()];
             let n: i32 = num.parse().unwrap();
